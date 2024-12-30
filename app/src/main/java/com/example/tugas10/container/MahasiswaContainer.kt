568d63b1b233d2d60b338f34c2ddc11281014dc9
djafarxyxy <farhanch12344@gmail.com>
2024-12-30 12:59:41 +0700

    membuat mahasiswa container

---
+++ app/src/main/java/com/example/tugas10/container/MahasiswaContainer.kt
@@ -0,0 +1,30 @@
+package com.example.tugas10.container
+
+import com.example.tugas10.repository.MahasiswaRepository
+import com.example.tugas10.repository.NetworkMahasiswaRepository
+import com.example.tugas10.service.MahasiswaService
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+
+interface AppContainer{
+    val mahasiswaRepository: MahasiswaRepository
+}
+
+class MahasiswaContainer : AppContainer {
+
+    private val baseUrl = "http://10.0.2.2/umyTI/" // localhost diganti ip kalo run di hp
+    private val json = Json { ignoreUnknownKeys = true }
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+        .baseUrl(baseUrl).build()
+
+    private val mahasiswaService: MahasiswaService by lazy {
+        retrofit.create(MahasiswaService::class.java)
+    }
+
+    override val mahasiswaRepository: MahasiswaRepository by lazy {
+        NetworkMahasiswaRepository(mahasiswaService)
+    }
+}
