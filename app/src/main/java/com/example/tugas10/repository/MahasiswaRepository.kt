bee73899fdb2b5af021d630057634d2bb7a83219
djafarxyxy <farhanch12344@gmail.com>
2024-12-30 13:03:10 +0700

    membuat mahasiswa repository

---
+++ app/src/main/java/com/example/tugas10/repository/MahasiswaRepository.kt
@@ -0,0 +1,50 @@
+package com.example.tugas10.repository
+
+import com.example.tugas10.model.Mahasiswa
+import com.example.tugas10.service.MahasiswaService
+import java.io.IOException
+
+interface MahasiswaRepository{
+    suspend fun getMahasiswa(): List<Mahasiswa>
+
+    suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
+
+    suspend fun updateMahasiswa(nim: String, mahasiswa: Mahasiswa)
+
+    suspend fun deleteMahasiswa(nim: String)
+
+    suspend fun getMahasiswaById(nim: String): Mahasiswa
+}
+
+class NetworkMahasiswaRepository(
+    private val mahasiswaService: MahasiswaService
+) : MahasiswaRepository {
+    override suspend fun insertMahasiswa(mahasiswa: Mahasiswa) {
+        mahasiswaService.insertMahasiswa(mahasiswa)
+    }
+
+    override suspend fun updateMahasiswa(nim: String, mahasiswa: Mahasiswa) {
+        mahasiswaService.updateMahasiswa(nim, mahasiswa)
+    }
+
+    override suspend fun deleteMahasiswa(nim: String) {
+        try {
+            val response = mahasiswaService.deleteMahasiswa(nim)
+            if (!response.isSuccessful) {
+                throw IOException("Failed to delete mahasiswa. HTTP Status code: " +
+                        "${response.code()}")
+            }else{
+                response.message()
+                println(response.message())
+            }
+        } catch (e:Exception){
+            throw e
+        }
+    }
+
+    override suspend fun getMahasiswaById(nim: String): Mahasiswa {
+        return mahasiswaService.getMahasiswaById(nim)
+    }
+
+    override suspend fun getMahasiswa(): List<Mahasiswa> = mahasiswaService.getMahasiswa()
+}
